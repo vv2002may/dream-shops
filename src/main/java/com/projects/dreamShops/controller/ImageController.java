65e20338e895287ceb9083a0373eebafcd43ab93
Vimal Verma <vimalverma@Vimals-MacBook-Air.local>
2025-02-04 10:17:47 +0530

    resolved issue of infinite printing of Category & Product

--- src/main/java/com/projects/dreamShops/controller/ImageController.java
+++ src/main/java/com/projects/dreamShops/controller/ImageController.java
@@ -1,10 +1,13 @@
 package com.projects.dreamShops.controller;
 
+import java.net.http.HttpHeaders;
 import java.util.List;
 
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +23,7 @@ import com.projects.dreamShops.exchange.response.image.ImageResponse;
 import com.projects.dreamShops.model.Image;
 import com.projects.dreamShops.services.image.IImageService;
 
+import io.swagger.v3.oas.models.media.MediaType;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -41,26 +45,26 @@ public class ImageController {
         }
     }
 
-    // @GetMapping("/image/download/{imageId}")
-    // public ResponseEntity<?> downloadImage(@PathVariable Long imageId) {
-    //     try {
-    //         Image image = imageService.getImageById(imageId);
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<?> downloadImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
 
-    //         ByteArrayResource resource = new ByteArrayResource(
-    //                 image.getImage().getBytes(1, (int) image.getImage().length()));
+            ByteArrayResource resource = new ByteArrayResource(
+                    image.getImage().getBytes(1, (int) image.getImage().length()));
 
-    //         return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
-    //                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
-    //                 .body(resource);
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+                    .body(resource);
 
-    //     } catch (ImageNotFoundException e) {
-    //         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-    //                 .body(new ApiResponse(e.getMessage(), null));
-    //     } catch (Exception e) {
-    //         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-    //                 .body(new ApiResponse("Download failed", null));
-    //     }
-    // }
+        } catch (ImageNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Download failed", null));
+        }
+    }
 
     @PutMapping("/image/update/{imageId}")
     public ResponseEntity<ApiResponse> updateImage(@RequestBody MultipartFile file, @PathVariable Long imageId) {
